e43026c95432d5093288a19b3191a32308add71f
Jeevanandam A <jeevanandam2708@gmail.com>
2025-07-20 09:47:34 +0530

    booking details

--- backend/models/booking.js
+++ backend/models/booking.js
@@ -1,35 +1,34 @@
 import mongoose from "mongoose";
 
-const bookinSchema = new mongoose.Schema({
-    user : {
-        type : String,
-        required : true,
-        ref : "User"
-    },
-    show : {
-        type : String,
-        required : true,
-        ref : "Show"
-    },
-    amount : {
-        type : Number,
-        required : true,
-    },
-    bookedSeats : {
-        type : [String],
-        required : true,
-    },
-    isPaid : {
-        type : Boolean,
-        default : false,
-    },
-    paymentLink : {
-        type : String,
-    },
-},{
-    timestamps : true
-})
+const bookingSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User"
+  },
+  show: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Show"
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  bookedSeats: {
+    type: [String],
+    required: true,
+  },
+  isPaid: {
+    type: Boolean,
+    default: false,
+  },
+  paymentLink: {
+    type: String,
+  },
+}, {
+  timestamps: true
+});
 
-const Booking = mongoose.model("Booking" , bookinSchema);
-
-export default Booking
+const Booking = mongoose.model("Booking", bookingSchema);
+export default Booking;
